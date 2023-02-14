16007c6800ab15403a393640b3ad698d44fe8388
Minna Lehtimaki <minna.lehtimaki@edu.karelia.fi>
2023-02-14 18:49:10 +0200

    Lisätty c++ luokat palloille ja erillinen fysiikkamateriaali

---
+++ Source/Pool3D/Core/MCueBallBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MBallBase.h"
+#include "MCueBallBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class POOL3D_API AMCueBallBase : public AMBallBase
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/Pool3D/Core/MRedBallBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MBallBase.h"
+#include "MRedBallBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class POOL3D_API AMRedBallBase : public AMBallBase
+{
+	GENERATED_BODY()
+	
+};
