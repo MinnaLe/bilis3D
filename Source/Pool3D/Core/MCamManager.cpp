452122d08b64fa512d653ce12f8feb877d470efd
Minna Lehtimaki <minna.lehtimaki@edu.karelia.fi>
2023-02-20 20:53:50 +0200

    Ensimmäinen versio CueBallista

--- Source/Pool3D/Core/MCamManager.cpp
+++ Source/Pool3D/Core/MCamManager.cpp
@@ -5,6 +5,9 @@
 
 #include "Camera/CameraComponent.h"
 #include "GameFramework/SpringArmComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "Kismet/KismetMathLibrary.h"
+//using namespace AMCamManager;
 
 
 // Sets default values
@@ -28,6 +31,7 @@ AMCamManager::AMCamManager()
 void AMCamManager::BeginPlay()
 {
 	Super::BeginPlay();
+	UGameplayStatics::GetPlayerController(GetWorld(), 0)->SetShowMouseCursor(true);
 	
 }
 
@@ -39,6 +43,21 @@ void AMCamManager::CameraViewUpdate()
 }
 
 
+void AMCamManager::OnTouchSwipe()
+{
+	if (bShotInitiated)
+	{
+	FHitResult HitResult;
+	UGameplayStatics::GetPlayerController(GetWorld(), 0)->GetHitResultUnderCursorByChannel(ETraceTypeQuery::TraceTypeQuery1, false, HitResult);
+	FVector endLocation = HitResult.Location;
+	endLocation.Z = CueBallObj->GetActorLocation().Z;
+
+	FHitResult SphereHitResult;
+	UKismetSystemLibrary::SphereTraceSingle(GetWorld(), ShotStartLocation, endLocation, 1.3f, ETraceTypeQuery::TraceTypeQuery1, false, TArray<AActor*>{CueBallObj},
+		EDrawDebugTrace::ForOneFrame, SphereHitResult, true);
+	ShotEndLocation = SphereHitResult.Location;
+	}
+}
 
 
 
@@ -47,13 +66,47 @@ void AMCamManager::CameraViewUpdate()
 void AMCamManager::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+	if (bTouchInitiated)
+		OnTouchSwipe();
+}
+
+void AMCamManager::TouchPressed(ETouchIndex::Type TouchType, FVector NewTouchLocation)
+{
+	UE_LOG(LogTemp, Warning, TEXT("Touch pressed"));
+	bTouchInitiated = true;
+	FHitResult HitResult;
+	UGameplayStatics::GetPlayerController(GetWorld(), 0)->GetHitResultUnderCursorByChannel(ETraceTypeQuery::TraceTypeQuery1, false, HitResult);
+	if (Cast<AMCueBallBase>(HitResult.GetActor()))
+	{
+		ShotStartLocation = HitResult.Location;
+		bShotInitiated = true;
+	}
 
 }
+
+void AMCamManager::TouchReleased(ETouchIndex::Type TouchType, FVector ReleasedTouchLocation)
+{
+	UE_LOG(LogTemp, Warning, TEXT("Touch released"));
+	bTouchInitiated = false;
+	if (bShotInitiated)
+	{
+		FVector Force = (ShotEndLocation - ShotStartLocation);
+		UKismetMathLibrary::Vector_Normalize(Force);
+		CueBallObj->SM_Ball->AddForce(Force * ShotForceConst, NAME_None, true);
+		bShotInitiated = false;
+	}
+}
+
+
+
 
 // Called to bind functionality to input
 void AMCamManager::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 	PlayerInputComponent->BindAction(FName("CameraViewChange"), EInputEvent::IE_Pressed, this, &AMCamManager::CameraViewUpdate);
+	PlayerInputComponent->BindTouch(EInputEvent::IE_Pressed, this, &AMCamManager::TouchPressed);
+	PlayerInputComponent->BindTouch(EInputEvent::IE_Released, this, &AMCamManager::TouchReleased);
+
 }
 
